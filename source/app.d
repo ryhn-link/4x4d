e286a82596b2ca1ee6aef6e72698dc86f07359c1
Ryhon <ryhon0@gmail.com>
2021-11-16 23:24:38 +0100

    Client login

--- source/app.d
+++ source/app.d
@@ -1,2 +1,12 @@
 import std.stdio;
+import matrix;
+import std.process;
 
+// Test for the library, this will be removed
+void main()
+{
+	auto mx = new MatrixClient("https://ryhn.link");
+	mx.login("rbot", environment["PASSWORD"]);
+
+	writeln(mx.user_id);
+}
---
+++ source/matrix.d
@@ -0,0 +1,75 @@
+module matrix;
+import std.json;
+import std.net.curl;
+import std.format : format;
+
+class MatrixClient
+{
+private:
+	static const string[string] NULL_PARAMS;
+
+	string accessToken;
+
+	string buildUrl(string endpoint, const string[string] params = NULL_PARAMS,
+			string apiVersion = "unstable", string section = "client")
+	{
+		string url = "%s/_matrix/%s/%s/%s".format(this.homeserver, section, apiVersion, endpoint);
+		char concat = '?';
+
+		if (this.accessToken.length)
+		{
+			url ~= "%caccess_token=%s".format(concat, this.accessToken);
+			concat = '&';
+		}
+
+		string paramString = this.makeParamString(params, concat);
+		if (paramString.length) url ~= paramString;
+
+		return url;
+	}
+
+	string makeParamString(const string[string] params, char concat)
+	{
+		if (params.length == 0)
+		{
+			return "";
+		}
+		string result = "%s".format(concat);
+		foreach (key, value; params)
+		{
+			result ~= "%s=%s&".format(key, value);
+		}
+		return result[0 .. $ - 1];
+	}
+
+public:
+	string homeserver, user_id;
+
+	this(string homeserver = "https://matrix.org")
+	{
+		this.homeserver = homeserver;
+
+		// Check well known matrix
+	}
+
+	void login(string user, string password)
+	{
+		string url = buildUrl("login");
+		JSONValue req = JSONValue();
+		req["type"] = "m.login.password";
+		req["user"] = user;
+		req["password"] = password;
+
+		string reqs = req.toString();
+
+		JSONValue resp = parseJSON(post(url, reqs));
+		
+		this.accessToken = resp["access_token"].str;
+		this.user_id = resp["user_id"].str;
+	}
+
+	void sync()
+	{
+
+	}
+}
